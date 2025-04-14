c04869dde97d5c0f7bd9ec23de2b8618152635e5
Ayoub Chaoui <78933570+ayoubchaoui@users.noreply.github.com>
2025-04-14 23:22:24 +0100

    Add files via upload

---
+++ Projcet 2/meto_analytics_engineering/terraform/main.tf
@@ -0,0 +1,52 @@
+# =========================================================================================================
+# Create Virtual machine (incl. SSH and GC CLI authentication, and initial installation of some libraries)
+# =========================================================================================================
+
+resource "google_compute_instance" "vm_instance" {
+  name         = var.instance_name
+  machine_type = var.machine_type
+  zone         = var.zone
+  project      = var.project_id
+
+  boot_disk {
+    initialize_params {
+      image = "projects/ubuntu-os-cloud/global/images/ubuntu-2004-focal-v20240519"
+      size  = 30
+      type  = "pd-balanced"
+    }
+  }
+
+  network_interface {
+    network    = "default"
+    subnetwork = "default"
+    access_config {
+      network_tier = "PREMIUM"
+    }
+  }
+
+  service_account {
+    email = 
+    scopes = ["cloud-platform"]
+  }
+
+  metadata = {
+    sshKeys = "${var.gce_ssh_user}:${file(var.ssh_pub_key_file)}"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = var.gce_ssh_user
+      host        = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+      private_key = file(var.ssh_priv_key_file)
+    }
+    inline = [
+      "git clone https://github.com/ayoub/meto_analytics_engineering.git  ",
+      "sudo apt-get install wget",
+      "wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh",
+      "bash Anaconda3-2022.10-Linux-x86_64.sh -b -p /home/ayoub/anaconda3",
+      "rm Anaconda3-2022.10-Linux-x86_64.sh",
+      "export PATH=/home/ayoub/anaconda3/bin:$PATH"
+    ]
+  }
+}
---
+++ Projcet 2/meto_analytics_engineering/terraform/variables.tfvars
@@ -0,0 +1,8 @@
+
+project_id = "crypto-analytics-2404"
+region = "europe-west6"
+zone = "europe-west6-a"
+
+gce_ssh_user = "aayomide"
+ssh_pub_key_file = "C:/Users/pc/.ssh/ssh_key.pub"
+ssh_priv_key_file = "C:/Users/pc/.ssh/ssh_key"
